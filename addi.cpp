385896821a33e2b55d23642e3ed118fe2f0380fb
Madhuri-Golde <103272424+Madhuri-Golde@users.noreply.github.com>
2022-04-10 10:02:42 +0530

    This is my first commit with my original file

---
+++ addi.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a,b,sub,add,mul,div;
+	cout <<"enter the two numbers:";
+	cin>>a>>b;
+	
+	add=a+b;
+	sub=a-b;
+	mul=a*b;
+	div=a/b;
+	
+	cout<<"sum= "<<add<<"\nsub= "<<sub<<"\nmul= "<<mul<<"\ndiv= "<<div;
+	
+
+}
